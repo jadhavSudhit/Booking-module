82b2b8baa2b37c9b2ba292d3a042621d7acd0008
jadhavSudhit <88421611+jadhavSudhit@users.noreply.github.com>
2024-08-01 23:36:43 +0530

    stay.model.js

---
+++ stay.model.js
@@ -0,0 +1,12 @@
+// models/stay.js
+const Stay = {
+  roomId: 0,
+  stayDateFrom: 'YYYY-MM-DD',
+  stayDateTo: 'YYYY-MM-DD',
+  arrivalDays: [],
+  departureDays: [],
+  minStay: 0,
+  maxStay: 0,
+};
+
+module.exports = Stay;
