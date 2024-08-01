77ac03b1362c254759882ad9c165de70e607eb74
jadhavSudhit <88421611+jadhavSudhit@users.noreply.github.com>
2024-08-01 23:41:16 +0530

    Update customerRoutes.js

--- customerRoutes.js
+++ customerRoutes.js
@@ -2,4 +2,11 @@
 const express = require('express');
 const customerController = require('../controllers/customerController');
 
-const router = express
+const router = express.Router();
+
+router.get('/', customerController.getCustomers);
+router.post('/', customerController.createCustomer);
+router.put('/:customerId', customerController.updateCustomer);
+router.delete('/:customerId', customerController.deleteCustomer);
+
+module.exports = router;
