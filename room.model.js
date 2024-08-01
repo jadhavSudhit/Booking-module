a1817deac5950210e01851e5afa82184de5dbe09
jadhavSudhit <88421611+jadhavSudhit@users.noreply.github.com>
2024-08-01 23:35:15 +0530

    room.model.js

---
+++ room.model.js
@@ -0,0 +1,11 @@
+// models/room.js
+const Room = {
+  roomId: 0,
+  locationId: 0,
+  locationName: '',
+  roomName: '',
+  pricePerDayPerPerson: 0,
+  guestCapacity: 0,
+};
+
+module.exports = Room;
