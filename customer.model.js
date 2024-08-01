e9aef2ec5410e17dcda44fce702f5dedc8cddd1c
jadhavSudhit <88421611+jadhavSudhit@users.noreply.github.com>
2024-08-01 23:37:43 +0530

    customer.model.js

---
+++ customer.model.js
@@ -0,0 +1,16 @@
+// models/customer.js
+const Customer = {
+  customerId: 0,
+  age: 0,
+  birthDate: 'YYYY-MM-DD',
+  firstName: '',
+  middleName: '',
+  lastName: '',
+  country: '',
+  state: '',
+  city: '',
+  pinCode: 0,
+  initialAddress: '',
+};
+
+module.exports = Customer;
